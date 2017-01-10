20cb94a4801875c0095892de6dad470c6caee371
Tateo Okubo <neteooo@gmail.com>
2017-01-10 00:24:29 +0000

    update want, have method

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,23 @@ class User < ActiveRecord::Base
   has_many :followed_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followed_users, through: :followed_relationships, source: :follower
 
+  # through:で指定されるのがDB上の中間テーブル
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
+  
+  # ownershipsテーブルからtypeがWantであるものを取得
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  # wantしたアイテムの一覧を取得できる
+  has_many :want_items , through: :wants, source: :item
 
+  # ownershipsテーブルからtypeがHaveであるものの一覧がhaves
+  # haveしたアイテムの一覧がhave_itemsで取得可能
+  # source: :itemとしている部分はthrough: :havesで指定した参照先のクラスHaveに宣言されている
+  # belongs\to :itemのitemを取得することを意味
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items , through: :haves, source: :item
+  
+  
 
   # 他のユーザーをフォローする
   def follow(other_user)
@@ -30,21 +44,33 @@ class User < ActiveRecord::Base
   end
 
   ## TODO 実装
+  # itemをhaveする
   def have(item)
+    have_items.create(item_id: item.id)
   end
 
+  # itemのhaveを解除する
   def unhave(item)
+    have_items.find_by(item_id: item.id).destroy
   end
 
+  # itemをhaveしている場合true、haveしていない場合falseを返す
   def have?(item)
+    have_items.include?(item)
   end
-
+  
+  # itemをwantする
   def want(item)
+    want_items.create(item_id: item.id)
   end
 
+  # itemのwantを解除する
   def unwant(item)
+    want_items.find_by(item_id: item.id).destroy
   end
 
+  #	itemをwantしている場合true、wantしていない場合falseを返す
   def want?(item)
+    want_items.include?(item)
   end
 end
