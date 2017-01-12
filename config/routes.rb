5ce934c80e7930198fd8e3e5015a18d1e2f6af12
Tateo Okubo <neteooo@gmail.com>
2017-01-12 11:27:21 +0000

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,11 +13,11 @@ Rails.application.routes.draw do
   resources :ownerships, only: [:create, :destroy]
   resources :items , only: [:new , :show]
   
-  resources :ranking do
-    
-  end #, only: [:show]
-
-  
+  # ルーティングの設定
+  namespace :ranking do
+    get :have
+    get :want
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
