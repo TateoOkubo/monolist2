00f0c20365457b350cfddbc7c740ec22d8133239
Tateo Okubo <neteooo@gmail.com>
2017-01-13 05:32:34 +0000

    add ranking

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,7 +1,21 @@
 class RankingController < ApplicationController
   def have
+    @title = 'Haveランキング'
+    have_rank =  Have.group(:item_id).order('count_all desc').limit(10).count.keys
+
+    @items = Item.find(have_rank).sort_by{ |o| have_rank.index(o.id) }
+    render 'ranking'
   end
 
   def want
+    @title = 'Want ランキング'
+    #Want.group(:item_id).count()でitem_idごとの数を検索可能
+    # limit(最大取得行数)でレコード件数を指定
+    want_rank =  Want.group(:item_id).order('count_all desc').limit(10).count.keys
+    
+    @items = Item.find(want_rank).sort_by{ |o| want_rank.index(o.id) }
+
+    render 'ranking'
   end
 end
+
--- app/models/want.rb
+++ app/models/want.rb
@@ -1,2 +1,7 @@
 class Want < Ownership
+  belongs_to :user
+  belongs_to :item
+  
+  #Want.group(:item_id).count()でitem_idごとの数を検索可能
+  
 end
