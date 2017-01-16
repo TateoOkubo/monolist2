f7ecfa51c010e76e348febfdf0d72ddef68621cd
Tateo Okubo <neteooo@gmail.com>
2017-01-16 16:06:34 +0000

    add num

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,9 +1,10 @@
 class RankingController < ApplicationController
   def have
     @title = 'Haveランキング'
-    have_rank =  Have.group(:item_id).order('count_all desc').limit(10).count.keys
+    have_rank =  Have.group(:item_id).order('count_all desc').limit(10).count
 
-    @items = Item.find(have_rank).sort_by{ |o| have_rank.index(o.id) }
+    @items = Item.find(have_rank.keys).sort_by{ |o| have_rank.keys.index(o.id) }
+    @ranks = have_rank.values
     render 'ranking'
   end
 
@@ -15,6 +16,7 @@ class RankingController < ApplicationController
     
     @items = Item.find(want_rank).sort_by{ |o| want_rank.index(o.id) }
 
+    @ranks =  Want.group(:item_id).order('count_item_id desc').limit(10).count('item_id').values
     render 'ranking'
   end
 end
